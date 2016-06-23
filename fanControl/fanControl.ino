62a4d04be94e3d365dc4decb5b85412345d1ae03
Koen Schoute <koen@gorgabal.nl>
2016-06-23 16:47:13 +0200

    fancontrol for 3 fans. UNTESTED

--- fanControl/fanControl.ino
+++ fanControl/fanControl.ino
@@ -1,35 +1,56 @@
-int fanPulse = 0;
+#define fanPulse1 A0
+#define fanPulse2 A1
+#define fanPulse3 A2
+
+#define fanControl1 3
+#define fanControl2 5
+#define fanControl3 6
+
 unsigned long pulseDuration;
 
 void setup()
 {
-	Serial.begin(9600);
-	pinMode(fanPulse, INPUT);
-	digitalWrite(fanPulse,HIGH);
+  Serial.begin(9600);
+  pinMode(fanPulse1, INPUT);
+  pinMode(fanPulse2, INPUT);
+  pinMode(fanPulse3, INPUT);
+  digitalWrite(fanPulse1,HIGH);
+  digitalWrite(fanPulse2,HIGH);
+  digitalWrite(fanPulse3,HIGH);
 }
 
-void readPulse() {
-	pulseDuration = pulseIn(fanPulse, LOW);
-	double frequency = 1000000/pulseDuration;
+void readPulse(int fanPulse) {
+  Serial.print("FanPulse");
+  Serial.println(fanPulse);
+  pulseDuration = pulseIn(fanPulse, LOW);
+  double frequency = 1000000/pulseDuration;
 
-	Serial.print("pulse duration:");
-	Serial.println(pulseDuration);
+  Serial.print("pulse duration:");
+  Serial.println(pulseDuration);
 
-	Serial.print("time for full rev. (microsec.):");
-	Serial.println(pulseDuration*2);
-	Serial.print("freq. (Hz):");
-	Serial.println(frequency/2);
-	Serial.print("RPM:");
-	Serial.println(frequency/2*60);
+  Serial.print("time for full rev. (microsec.):");
+  Serial.println(pulseDuration*2);
+  Serial.print("freq. (Hz):");
+  Serial.println(frequency/2);
+  Serial.print("RPM:");
+  Serial.println(frequency/2*60);
 
 }
 
 void loop()
 {
-	analogWrite(3,20);
+	analogWrite(fanControl1,20);
+	analogWrite(fanControl2,20);
+	analogWrite(fanControl3,20);
 	delay(5000);
-	readPulse();
-	analogWrite(3,255);
+	readPulse(fanPulse1);
+	readPulse(fanPulse2);
+	readPulse(fanPulse3);
+	analogWrite(fanControl1,255);
+	analogWrite(fanControl2,255);
+	analogWrite(fanControl3,255);
 	delay(5000);
-	readPulse();
+  readPulse(fanPulse1);
+  readPulse(fanPulse2);
+  readPulse(fanPulse3);
 }
