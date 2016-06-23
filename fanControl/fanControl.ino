be07e8ac040df31ac7c33ca17be92eb0a5806f3c
Koen Schoute <koen@gorgabal.nl>
2016-06-23 21:09:19 +0200

    started reading buttons, simplyfied code

--- fanControl/fanControl.ino
+++ fanControl/fanControl.ino
@@ -1,56 +1,47 @@
-#define fanPulse1 A0
-#define fanPulse2 A1
-#define fanPulse3 A2
+#include <Bounce2.h>
+#define relayPin 1
+#define onButton 2
+#define offButton 3
 
-#define fanControl1 3
-#define fanControl2 5
-#define fanControl3 6
 
-unsigned long pulseDuration;
+Bounce onBounce = Bounce();  
+Bounce offBounce = Bounce();  
 
 void setup()
 {
+
   Serial.begin(9600);
-  pinMode(fanPulse1, INPUT);
-  pinMode(fanPulse2, INPUT);
-  pinMode(fanPulse3, INPUT);
-  digitalWrite(fanPulse1,HIGH);
-  digitalWrite(fanPulse2,HIGH);
-  digitalWrite(fanPulse3,HIGH);
+  pinMode(relayPin, OUTPUT);
+ 
+ 
+  pinMode(onButton, INPUT);
+  digitalWrite(onButton, LOW); 
+  onBounce.attach(onButton);
+  onBounce.interval(5);
+
+
+  pinMode(offButton, INPUT);
+  digitalWrite(offButton, LOW);
+  offBounce.attach(offButton);
+  offBounce.interval(5);
+  
+
+  
+  Bounce bouncer02 = Bounce();  
 }
 
-void readPulse(int fanPulse) {
-  Serial.print("FanPulse");
-  Serial.println(fanPulse);
-  pulseDuration = pulseIn(fanPulse, LOW);
-  double frequency = 1000000/pulseDuration;
-
-  Serial.print("pulse duration:");
-  Serial.println(pulseDuration);
-
-  Serial.print("time for full rev. (microsec.):");
-  Serial.println(pulseDuration*2);
-  Serial.print("freq. (Hz):");
-  Serial.println(frequency/2);
-  Serial.print("RPM:");
-  Serial.println(frequency/2*60);
-
-}
 
 void loop()
 {
-	analogWrite(fanControl1,20);
-	analogWrite(fanControl2,20);
-	analogWrite(fanControl3,20);
-	delay(5000);
-	readPulse(fanPulse1);
-	readPulse(fanPulse2);
-	readPulse(fanPulse3);
-	analogWrite(fanControl1,255);
-	analogWrite(fanControl2,255);
-	analogWrite(fanControl3,255);
-	delay(5000);
-  readPulse(fanPulse1);
-  readPulse(fanPulse2);
-  readPulse(fanPulse3);
+
+  onBounce.update();
+  offBounce.update();
+  if(offBounce.read() == true){
+	digitalWrite(relayPin, LOW);
+  } else {
+    if(onBounce.read() == true){
+  	digitalWrite(relayPin, HIGH);
+
+    }
+  }
 }
