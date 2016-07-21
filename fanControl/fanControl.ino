a1ed15394251f1463f882d8595ec387a759536c5
Koen Schoute <koen@gorgabal.nl>
2016-07-21 13:10:53 +0200

    unkown changes, please see diff

--- fanControl/fanControl.ino
+++ fanControl/fanControl.ino
@@ -1,47 +1,38 @@
 #include <Bounce2.h>
-#define relayPin 1
-#define onButton 2
+#define relayPin 8
+#define onButton 12
 #define offButton 3
+#define bounceInterval 5
+#define debugPin 13
+#define errorPin 7
 
-
-Bounce onBounce = Bounce();  
-Bounce offBounce = Bounce();  
+//Bounce onBounce = Bounce();  
+//Bounce offBounce = Bounce();  
 
 void setup()
 {
 
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
- 
- 
+  pinMode(errorPin, OUTPUT);
+   pinMode(debugPin, OUTPUT);
   pinMode(onButton, INPUT);
-  digitalWrite(onButton, LOW); 
-  onBounce.attach(onButton);
-  onBounce.interval(5);
 
-
-  pinMode(offButton, INPUT);
-  digitalWrite(offButton, LOW);
-  offBounce.attach(offButton);
-  offBounce.interval(5);
-  
-
-  
-  Bounce bouncer02 = Bounce();  
 }
 
 
 void loop()
 {
+  Serial.println("loop");
 
-  onBounce.update();
-  offBounce.update();
-  if(offBounce.read() == true){
-	digitalWrite(relayPin, LOW);
-  } else {
-    if(onBounce.read() == true){
+  if(digitalRead(onButton) == HIGH){
   	digitalWrite(relayPin, HIGH);
-
-    }
+  	digitalWrite(debugPin, HIGH);
+   Serial.println("on");
+  } else {
+       Serial.println("off");
+  	digitalWrite(debugPin, LOW);
+	digitalWrite(relayPin, LOW);
   }
+  
 }
