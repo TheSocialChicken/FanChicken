14dcceedda26bf154aec1acab631cc34e3e778f3
Koen Schoute <koen@gorgabal.nl>
2016-10-27 16:17:45 +0200

    fanstatus.h now compiles

--- fanControl/raspberry/fanstatus.h
+++ fanControl/raspberry/fanstatus.h
@@ -2,8 +2,8 @@
  * author: Koen Schoute, koen@gorgabal.nl (october 2016)
  * Provides internal communication template. 
  */
-#ifndef fanstatus_t
-#define fanstatus_t
+#ifndef FANSTATUS_H
+#define FANSTATUS_H
 
 	/*
 	 * The fanstatus struct is meant for holding information about the individual fansystems. 
@@ -12,20 +12,20 @@
 	 */
 	struct fanstatus_t {
 		 int id;
-		 std::string devicepath;
-		 bool:active;
-		 bool:error;
-		 float:temperature;
-		 float:humidity;
+		 char const *devicepath[50];
+		 bool active;
+		 bool error;
+		 float temperature;
+		 float humidity;
 
-		/*fanstatus_t(){
+		fanstatus_t(){
 			id = -1;
-			devicepath = "/dev/null";
+			devicepath[0] = "/dev/null";
 			active = false;
 			error = false;
 			temperature = 0.0;
 			humidity = 0.0;
-		}*/
+		}
 
 	};
 
