d212bd0f37a6ada4a2bb00cdee90c83a0cbdb7be
Koen Schoute <koen@gorgabal.nl>
2016-11-02 15:22:02 +0000

    code skeleton completed :D

---
+++ fanControl/raspberry/main.cpp
@@ -0,0 +1,16 @@
+
+#include "Communicator.h"
+#include "controlPanel.h"
+#include "fanStatusManager.h"
+#include "fanstatus.h"
+
+
+
+int main(int argc, char *argv[]){
+	
+	//todo imlementation. 
+	
+	assert(false);
+
+	return 0;
+}
