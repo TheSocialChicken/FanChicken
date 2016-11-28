c330ccd562793e79edc5a8dca01e74633187140b
Koen <koen@gorgabal.nl>
2016-11-28 11:41:16 +0100

    communication, implemented arduino side

--- fanControl/fanControl/fanControl.ino
+++ fanControl/fanControl/fanControl.ino
@@ -9,39 +9,98 @@
 //Bounce onBounce = Bounce();  
 //Bounce offBounce = Bounce();  
 
+//status variables
+
+bool fanActive = false;
+bool error = false;
+float temperature = -50;
+float humidity = -50;
+
+
 void setup()
 {
-
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
   pinMode(errorPin, OUTPUT);
    pinMode(debugPin, OUTPUT);
   pinMode(onButton, INPUT);
-
 }
 
 
 void loop()
 {
-  
-  char piSignal = 'x';
-  while(Serial.available() != 0){
-  	piSignal = Serial.read();
-  }
-
-if(piSignal != 'x'){
-  Serial.print("received: ");
-  Serial.println(piSignal);
-}
-  if(piSignal == '0'){
+  if(fanActive){
   	digitalWrite(relayPin, HIGH);
   	digitalWrite(debugPin, LOW);
-   Serial.println("on");
   } 
-  if(piSignal == '1'){
-  Serial.println("off");
-  digitalWrite(debugPin, HIGH);
+  if(!fanActive){
+  	digitalWrite(debugPin, HIGH);
 	digitalWrite(relayPin, LOW);
   }
+
+	updateStatus();
   
+}
+
+char piSignal;
+/*
+ * reads out sensors and serial line. 
+ * Sends information back to pi if requested
+ */
+void updateStatus(){
+
+  //TODO HUMIDITY SENSOR IMPLEMENTATION
+  //TODO TEMPERATURE SENSOR IMPLEMENTATION
+  
+  
+ 	while(Serial.available() != 0){
+ 		piSignal = Serial.read();
+  }
+
+	switch(piSignal){
+
+	case '0':
+		fanActive = true;
+		break;
+	case '1':
+		fanActive = false;	
+		break;
+	case '2':
+		sendStatus();
+		break;
+  case '3':
+    sendVerboseStatus();
+  case 'x':
+    //nothing received, do nothing
+    break;
+	default:
+	  Serial.print("received: ");
+	  Serial.println(piSignal);
+	}
+
+  piSignal = 'x';
+}
+
+void sendVerboseStatus(){
+  Serial.println();
+	Serial.print("fanActive:");
+	Serial.println(fanActive);
+	Serial.print("error:");
+	Serial.println(error);
+	Serial.print("temperature:");
+	Serial.println(temperature);
+	Serial.print("humidity:");
+	Serial.println(humidity);
+	Serial.println();
+}
+
+void sendStatus(){
+	Serial.print(fanActive);
+	Serial.print(',');
+	Serial.print(error);
+	Serial.print(',');
+	Serial.print(temperature);
+	Serial.print(',');
+	Serial.print(humidity);
+	Serial.print('\n');
 }
