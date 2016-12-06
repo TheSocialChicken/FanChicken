68e46acc611ea3115f736fd81be6309366c7d9d7
Koen <koen@gorgabal.nl>
2016-12-06 12:44:58 +0100

    NEEDS TESTING, arduino now reads out sensor

--- fanControl/fanControl/fanControl.ino
+++ fanControl/fanControl/fanControl.ino
@@ -1,34 +1,38 @@
-//#include <Bounce2.h>
+#include <DHT.h>
+
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
 #define relayPin 8
 #define onButton 12
 #define offButton 11
 #define bounceInterval 5
 #define debugPin 17
 #define errorPin 7
+#define DHTPIN 2 // Pin which is connected to the DHT sensor.
 
-//Bounce onBounce = Bounce();  
-//Bounce offBounce = Bounce();  
-
-//status variables
 
 bool fanActive = false;
 bool error = false;
 float temperature = -50;
 float humidity = -50;
+unsigned long SensorMilliSecCount= millis();
 
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
   pinMode(errorPin, OUTPUT);
-   pinMode(debugPin, OUTPUT);
+  pinMode(debugPin, OUTPUT);
   pinMode(onButton, INPUT);
+
+  dht.begin();
 }
 
 
 void loop()
 {
+
   if(fanActive){
   	digitalWrite(relayPin, HIGH);
   	digitalWrite(debugPin, LOW);
@@ -41,8 +45,6 @@ void loop()
       Serial.println("ERROR");
     }
   }
-
-
 	updateStatus();
   
 }
@@ -53,14 +55,10 @@ char piSignal;
  * Sends information back to pi if requested
  */
 void updateStatus(){
-
-  //TODO HUMIDITY SENSOR IMPLEMENTATION
-  //TODO TEMPERATURE SENSOR IMPLEMENTATION
   
-  
- 	while(Serial.available() != 0){
+	while(Serial.available() != 0){
  		piSignal = Serial.read();
-  }
+	}
 
 	switch(piSignal){
 
@@ -83,11 +81,13 @@ void updateStatus(){
 	  Serial.println(piSignal);
 	}
 
-  piSignal = 'x';
+	piSignal = 'x';
+
+	readDHT();
 }
 
 void sendVerboseStatus(){
-  Serial.println();
+	Serial.println();
 	Serial.print("fanActive:");
 	Serial.println(fanActive);
 	Serial.print("error:");
@@ -110,4 +110,14 @@ void sendStatus(){
 	Serial.print(',');
 	Serial.print(humidity);
 	Serial.print('\n');
+}
+
+//TODO NEEDS TESTING
+void readDHT(){
+	//self implemented non-blocking timer)
+	if(SensorMilliSecCount + 5000 < millis()){
+		humidity = dht.readHumidity();
+		temperature = dht.readTemperature();		
+	}
+
 }
